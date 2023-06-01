d62e13123052e7cc6a2a888073a2e1b41e2fccb5
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-06-01 07:55:18 +0530

    updated App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS ec2 instance !!!!" );
+        System.out.println( "Hello World from AWS EC2 instance !!!!" );
     }
 }
